dd9d2f75c46c1f307eb481498ddcad994394589a
prasanth13new <prasanth13new@gmail.com>
2022-11-29 23:34:00 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+dotenv.config()
+
+const port = process.env.PORT;
+
+const connect = async ()=>{
+try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to Mongodb!!..")
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongodb disconnected");
+})
+
+// middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.listen(port, ()=>{
+    connect()
+    console.log("Backend is connected!......")
+})
